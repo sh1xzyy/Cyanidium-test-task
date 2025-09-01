f35253b6fda9119b881f6e5b377e97961dfff585
sh1xzyy <99531559+sh1xzyy@users.noreply.github.com>
2025-09-01 04:18:28 +0300

    .

--- src/components/Tariffs/parts/TariffsCard.jsx
+++ src/components/Tariffs/parts/TariffsCard.jsx
@@ -15,7 +15,7 @@ const TariffsCard = ({
 	},
 }) => {
 	return (
-		<div
+		<li
 			className={clsx(
 				'relative flex flex-col justify-between py-[31px] px-[24px] w-full rounded-[28px] min-h-[430px] shadow-[inset_4px_6px_10px_4px_rgba(167,93,243,0.2)] md:px-[36px] md:pt-[36px] md:pb-[26px] xl:max-w-[360px]',
 				level === 'advanced' && 'bg-[#ffffff]',
@@ -37,7 +37,7 @@ const TariffsCard = ({
 			</div>
 
 			<ActionButton level={level} />
-		</div>
+		</li>
 	)
 }
 
